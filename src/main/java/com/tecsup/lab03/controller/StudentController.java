a518640fe80c2d023c6613b782fa529554c690e3
XxDavexXX <102324849+XxDavexXX@users.noreply.github.com>
2023-09-12 19:21:06 -0500

    Create StudentController.java

---
+++ src/main/java/com/tecsup/lab03/controller/StudentController.java
@@ -0,0 +1,9 @@
+public class StudentController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.print.out("Cambios Responsable Dave Santivañez");
+		
+	}
+
+}
