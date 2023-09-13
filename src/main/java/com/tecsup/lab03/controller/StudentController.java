8dd9d01ecad037fcc5d86679ec29f9e60348e721
XxDavexXX <102324849+XxDavexXX@users.noreply.github.com>
2023-09-12 19:25:24 -0500

    Delete src/main/java/com/tecsup/lab03/controller/StudentController.java

--- src/main/java/com/tecsup/lab03/controller/StudentController.java
+++
@@ -1,9 +0,0 @@
-public class StudentController {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.print.out("Cambios Responsable Dave Santivañez");
-		
-	}
-
-}
