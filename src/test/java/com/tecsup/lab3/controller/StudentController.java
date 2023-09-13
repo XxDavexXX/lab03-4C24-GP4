6cc8f3e45fa9af516212d8685e71791f1058707d
XxDavexX <dave.santivanez@gmail.com>
2023-09-12 19:32:38 -0500

    Tercer Commit

---
+++ src/test/java/com/tecsup/lab3/controller/StudentController.java
@@ -0,0 +1,11 @@
+package com.tecsup.lab3.controller;
+
+public class StudentController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Cambios Responsable Dave Santivañez");
+		
+	}
+
+}
