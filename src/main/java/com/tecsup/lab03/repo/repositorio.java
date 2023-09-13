a90fb32bb54afccc5916fef7d7c99599b824e84b
Alexander V. Hiñigo <anderson.vilcah@gmail.com>
2023-09-12 19:16:46 -0500

    Commit vilca

--- src/main/java/com/tecsup/lab03/repo/repositorio.java
+++ src/main/java/com/tecsup/lab03/repo/repositorio.java
@@ -1,7 +1,7 @@
-package com.tecsup.lab3.dto;
+package com.tecsup.lab03.repo;
 
-public class StudentDto {
-
+public class repositorio {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Alexander Vilca Hiñigo");
