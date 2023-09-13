c3a2346812524a6987b0c4ba33d283774f000e79
Alexander V. Hiñigo <anderson.vilcah@gmail.com>
2023-09-12 19:09:02 -0500

    Commit Alexander

---
+++ src/main/java/com/tecsup/lab3/dto/StudentDto.java
@@ -0,0 +1,10 @@
+package com.tecsup.lab3.dto;
+
+public class StudentDto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Student DTO");
+	}
+
+}
