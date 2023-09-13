a93d26e9264130137b608b067bc7513d4c0ec175
diegom-21 <jimmy.moreno@tecsup.edu.pe>
2023-09-12 19:00:09 -0500

    comit morenoo

--- src/main/java/com/tecsup/lab3/model/Student.java
+++ src/main/java/com/tecsup/lab3/model/Student.java
@@ -3,6 +3,6 @@ package com.tecsup.lab3.model;
 public class Student {
 	public static void main( String[] args )
     {
-        System.out.println( "Hello Class Student" );
+        System.out.println( "Hello Class Student Diego" );
     }
 }
